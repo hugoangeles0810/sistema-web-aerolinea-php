450caec3e9fc5acf35d318a4ceebdc6e49d6715d
Hugo Angeles <hugoangeles0810@gmail.com>
2014-04-24 08:59:19 -0500

    Agregando librerias css y js

--- Sistema/index.php
+++ Sistema/index.php
@@ -2,10 +2,25 @@
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title></title>
+        <title>.::Viajeros.com::.</title>
+        <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+        <script src="js/bootstrap.min.js"></script>
+        <script src="js/jquery.min.js"></script>
     </head>
     <body>
-        <h1>Tutorial de PHP</h1>
+        <header>
+            <h1>Viajeros.com</h1>
+            <h2>Viaja y disfruta</h2>
+        </header>
+        <nav>
+            <ul>
+                <li><a href="index.php">Inicio</a></li>
+                <li><a href="consulta.php">Consulta</a></li>
+                <li><a href="reserva.php">Reserva</a></li>
+                <li><a href="compra.php">Compra</a></li>
+                <li><a href="contactenos.php">Contáctenos</a></li>
+            </ul>
+        </nav>
 
     </body>
 </html>
