9adc19a73823c3d6f15befa3f1d651cc27c046de
Hugo Angeles <hugoangeles0810@gmail.com>
2014-04-23 22:06:26 -0500

    Creación del proyecto

---
+++ Sistema/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <h1>Tutorial de PHP</h1>
+
+    </body>
+</html>
