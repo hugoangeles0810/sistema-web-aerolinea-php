a4cb8aa78a684b281f7da1b185b96d8b13af0357
Hugo Angeles <hugoangeles0810@gmail.com>
2014-04-29 00:39:52 -0500

    Renderizando pagina principal 10%

--- Sistema/index.php
+++ Sistema/index.php
@@ -2,26 +2,66 @@
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title>.::Viajeros.com::.</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
-        <script src="js/bootstrap.min.js"></script>
         <script src="js/jquery.min.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+        <title>.::Viajeros.com::.</title>
     </head>
-    <body>
-        <div id="container">
+    <body >
+        <div class="container">
             <header>
-                <h1>Viajeros.com</h1>
-                <h2>Viaja y disfruta</h2>
-            </header>
-            <nav class="nav">
-                <ul>
-                    <li><a href="index.php">Inicio</a></li>
-                    <li><a href="consulta.php">Consulta</a></li>
-                    <li><a href="reserva.php">Reserva</a></li>
-                    <li><a href="compra.php">Compra</a></li>
-                    <li><a href="contactenos.php">Contáctenos</a></li>
-                </ul>
-            </nav>
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="navbar navbar-inverse" role="navigation">
+                            <div class="container">
+                                <nav>
+                                    <div class="navbar-header">
+                                        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".nav-collapse">
+                                            <span class="sr-only">Toggle navigation</span>
+                                            <span class="icon-bar"></span>
+                                            <span class="icon-bar"></span>
+                                            <span class="icon-bar"></span>
+                                        </button>
+                                        <a class="navbar-brand" href="#">Viajeros.com</a>
+                                    </div>
+                                    <div class="collapse navbar-collapse" id="menu">
+                                        <ul class="nav navbar-nav">
+                                            <li class="active"><a href="index.php">Inicio</a></li>
+                                            <li><a href="consulta.php">Consulta</a></li>
+                                            <li><a href="reserva.php">Reserva</a></li>
+                                            <li><a href="compra.php">Compra</a></li>
+                                            <li><a href="contactenos.php">Contáctenos</a></li>
+
+                                        </ul>
+                                        <ul class="nav navbar-form navbar-right">
+                                            <a class="btn btn-primary">Inicia Sesion</a>
+                                        </ul>
+
+                                    </div>
+                                </nav>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="well">
+                        <div class="container">
+                            <div class="col-md-6">
+                                <h1>Viajeros.com</h1>
+                                <h5 class="container">&nbsp;&nbsp;&nbsp;&nbsp;Viaja y disfruta!</h5>
+                            </div>
+                            <div class="col-md-3">
+                                <a href="#" class="thumbnail">
+                                    <img src="img/avion.jpg" alt="Viajeros.com">
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </header> 
         </div>
-    </body>
+
+    </div>
+</body>
 </html>
