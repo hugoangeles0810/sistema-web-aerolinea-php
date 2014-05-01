8d053c996f1b94f6a95638463c022be432189769
jhonnger <jhonny_94_17@hotmail.com>
2014-05-01 18:43:23 -0500

    Añadienco icono

--- Sistema/index.php
+++ Sistema/index.php
@@ -6,6 +6,7 @@
         <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
         <script src="js/jquery.min.js"></script>
         <script src="js/bootstrap.min.js"></script>
+        <link rel="shortcut icon" href="favicon.ico">
         <title>.::Viajeros.com::.</title>
     </head>
     <body >
